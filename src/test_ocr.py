9ae7b259720797287301e6b6420fc346b2580fe1
nivram108 <hchs010077@gmail.com>
2024-10-18 23:06:09 -0400

    update test ocr

--- src/test_ocr.py
+++ src/test_ocr.py
@@ -1,5 +1,5 @@
 from src.ocr.services.google_ocr_service import GoogleOcrService
 
 ocr_service = GoogleOcrService()
-file = 'C:\projects\sja\sja_processing\output\concat\\fb_interaction_count\concat_0.jpg'
-ocr_service.unit_image_ocr(file)
+file = 'your-image-path'
+print(ocr_service.unit_image_ocr(file)[0].description)
